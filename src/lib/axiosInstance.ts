91c571916d94d4da1067de952b109e4560977e40
bitbyte08 <me@bitworkspace.kr>
2025-10-27 10:02:00 +0900

    feat :: API 주소 연동

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000", // API 서버 기본 URL
+  baseURL: "https://subway.bitworkspace.kr", // API 서버 기본 URL
   timeout: 10000, // 10초 타임아웃
   headers: {
     "Content-Type": "application/json",
